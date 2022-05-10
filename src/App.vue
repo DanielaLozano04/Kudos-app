dfeb8e7f6a88b260e9a669d7f66b971e202fcc28
Daniela <danielalozano-04@hotmail.com>
2022-05-10 16:56:42 -0500

    configuración de proyecto
    Co-authored-by: Jesus <jesusfrancisco@hotmail.es>

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,22 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Titulo from './components/Titulo.vue'
+import Input from './components/Input.vue'
+import Button from './components/Button.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+<div class="w-96 m-auto text-center py-8 text-gray-800">
+  <Titulo msg="Gracias Jesus" />
+</div>
+<div class="w-96 border p-8 m-auto">
+  <Input/>
+  <Input/>
+  <Button/>
+</div>
+
+  
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/Button.vue
@@ -0,0 +1,5 @@
+<template>
+<button class="bg-red-700 text-white rounded w-full py-3 mt-8 ">
+    enviar
+</button>
+</template>
