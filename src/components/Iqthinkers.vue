5795a09a11668446dba26e64355617aad6e62679
Daniela <danielalozano-04@hotmail.com>
2022-05-12 17:44:06 -0500

    se reescribe componente de tabla
    Co-authored-by: Jesus <jesusfrancisco@hotmail.es>

--- src/components/Iqthinkers.vue
+++ src/components/Iqthinkers.vue
@@ -1,89 +1,97 @@
 <template>
   <div class="bg-[#F6F8F9] px-6 py-4 ml-8 mr-4 mt-4 mb-4">
+		<div class="flex items-center pb-4 place-content-between">
+			<span class="text-sm text-[#6E7C87] font-semibold uppercase"> 🔥 iqthinkers</span>
+			<button class="bg-white text-[10px] font-semibold text-[#6E7C87] rounded-md border border-[#D1D5DB] px-4 py-[4px]"> Add +</button>
+		</div>
 
-      <div class="flex place-content-between items-center pb-4">
-       <span class="text-sm text-[#6E7C87] font-semibold uppercase"> 🔥 iqthinkers</span>
-       <button class="bg-white text-[10px] font-semibold text-[#6E7C87] rounded-md border border-[#D1D5DB] px-4 py-[4px]"> Add +</button>
-      </div>
-
-      <div class=" border rounded-md border-[#D1D5DB] bg-white ">
-          <table >
-          <div class="grid gap-4 grid-cols-3 border-b bg-[#F9FAFB]">
-            <div class="text-[#6B7280] text-xs font-medium pl-6 py-3">NAME</div>
-            <div class="text-[#6B7280] text-xs font-medium pl-6 py-3">STATUS</div>
-            <div class="text-[#6B7280] text-xs font-medium pl-6 py-3">RECEIVED</div>
-            </div>
-
-            <div id="Daniel" class="grid gap-4 grid-cols-3 items-center border-b border-[#D1D5DB]">
-            <div class="pl-6 py-3 flex flex-row ">
-                <div class="pr-3 w-[40px] h-[40px] grid content-center">
-                    <img  src="/imagenes/JPG/Daniel.jpg" alt="Daniel">
-                </div>
-                <div class="grid grid-rows-2">
-                    <span class=" text-[#111827] font-normal text-sm">Daniel Rubiano</span>
-                    <span class=" text-[#6B7280] font-normal text-sm">Video/Audio</span>
-                </div>
-             </div>
-
-            <div class="flex pl-6 py-3">
-                <img src="/imagenes/SVG/Heart.svg" alt="Heart">
-                <img src="/imagenes/SVG/Heart.svg" alt="Heart">
-                <img src="/imagenes/SVG/Heart.svg" alt="Heart">
-            </div>
-            
-            <div class="flex pl-6 py-3 text-[#6B7280] font-normal text-sm" >6 #Kudos</div>
-            </div>
-
-
-
-            <div id="Daniela" class="grid gap-4 grid-cols-3 items-center border-b border-[#D1D5DB]">
-            <div class="pl-6 py-3 flex flex-row ">
-                <div class="pr-3 w-[40px] h-[40px] grid content-center">
-                    <img  src="/imagenes/JPG/Daniela.jpg" alt="Daniela">
-                </div>
-                <div class="grid grid-rows-2">
-                    <span class=" text-[#111827] font-normal text-sm">Daniela Lozano</span>
-                    <span class=" text-[#6B7280] font-normal text-sm">Designer</span>
-                </div>
-             </div>
-
-            <div class="flex pl-6 py-3">
-                <img src="/imagenes/SVG/Heart.svg" alt="Heart">
-                <img src="/imagenes/SVG/Heart.svg" alt="Heart">
-                <img src="/imagenes/SVG/Heart-1.svg" alt="Heart">
-            </div>
-            
-            <div class="flex pl-6 py-3 text-[#6B7280] font-normal text-sm" >3 #Kudos</div>
-            </div>
-
-
-            <div id="Emmanuel" class="grid gap-4 grid-cols-3 items-center border-b border-[#D1D5DB]">
-            <div class="pl-6 py-3 flex flex-row ">
-                <div class="pr-3 w-[40px] h-[40px] grid content-center">
-                    <img  src="/imagenes/JPG/Emma.jpg" alt="Emma">
-                </div>
-                <div class="grid grid-rows-2">
-                    <span class=" text-[#111827] font-normal text-sm">Emmanuel Deossa</span>
-                    <span class=" text-[#6B7280] font-normal text-sm">Lead Developer</span>
-                </div>
-             </div>
-
-            <div class="flex pl-6 py-3">
-                <img src="/imagenes/SVG/Heart.svg" alt="Heart">
-                <img src="/imagenes/SVG/Heart.svg" alt="Heart">
-                <img src="/imagenes/SVG/Heart-1.svg" alt="Heart">
-            </div>
-            
-            <div class="flex pl-6 py-3 text-[#6B7280] font-normal text-sm" >5 #Kudos</div>
-            </div>
-
-
-       
-        </table>
-
-      </div>
-
-      
-
+		<div class="overflow-hidden rounded-md shadow ring-1 ring-black ring-opacity-5">
+			<table class="w-full divide-y divide-[#D1D5DB] text-left">
+				<thead class="bg-[#F9FAFB]">
+					<tr>
+						<th class="text-[#6B7280] text-xs font-medium pl-6 py-3">
+							NAME
+						</th>
+						<th class="text-[#6B7280] text-xs font-medium pl-6 py-3">
+							STATUS
+						</th>
+						<th class="text-[#6B7280] text-xs font-medium pl-6 py-3">
+							RECEIVED
+						</th>
+					</tr>
+				</thead>
+				<tbody class="divide-y divide-[#D1D5DB] bg-white">
+					<tr>
+						<td>
+							<div class="flex flex-row py-3 pl-6 ">
+								<div class="pr-3 w-[40px] h-[40px] grid content-center">
+									<img  src="/imagenes/JPG/Daniel.jpg" alt="Daniel">
+								</div>
+								<div class="grid grid-rows-2">
+									<span class="text-[#111827] font-normal text-sm">Daniel Rubiano</span>
+									<span class="text-[#6B7280] font-normal text-sm">Video/Audio</span>
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="flex py-3 pl-6">
+								<img src="/imagenes/SVG/Heart.svg" alt="Heart">
+								<img src="/imagenes/SVG/Heart.svg" alt="Heart">
+								<img src="/imagenes/SVG/Heart.svg" alt="Heart">
+							</div>
+						</td>
+						<td>
+							<span class="pl-6 py-3 text-[#6B7280] font-normal text-sm" >6 #Kudos</span>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<div class="flex flex-row py-3 pl-6 ">
+								<div class="pr-3 w-[40px] h-[40px] grid content-center">
+									<img  src="/imagenes/JPG/Daniela.jpg" alt="Daniela">
+								</div>
+								<div class="grid grid-rows-2">
+									<span class=" text-[#111827] font-normal text-sm">Daniela Lozano</span>
+									<span class=" text-[#6B7280] font-normal text-sm">Designer</span>
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="flex py-3 pl-6">
+								<img src="/imagenes/SVG/Heart.svg" alt="Heart">
+								<img src="/imagenes/SVG/Heart.svg" alt="Heart">
+								<img src="/imagenes/SVG/Heart.svg" alt="Heart">
+							</div>
+						</td>
+						<td>
+							<span class="pl-6 py-3 text-[#6B7280] font-normal text-sm" >6 #Kudos</span>
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<div class="flex flex-row py-3 pl-6 ">
+								<div class="pr-3 w-[40px] h-[40px] grid content-center">
+									<img  src="/imagenes/JPG/Emma.jpg" alt="Emma">
+								</div>
+								<div class="grid grid-rows-2">
+									<span class=" text-[#111827] font-normal text-sm">Emmanuel Deossa</span>
+									<span class=" text-[#6B7280] font-normal text-sm">Lead Developer</span>
+								</div>
+							</div>
+						</td>
+						<td>
+							<div class="flex py-3 pl-6">
+								<img src="/imagenes/SVG/Heart.svg" alt="Heart">
+								<img src="/imagenes/SVG/Heart.svg" alt="Heart">
+								<img src="/imagenes/SVG/Heart.svg" alt="Heart">
+							</div>
+						</td>
+						<td>
+							<span class="pl-6 py-3 text-[#6B7280] font-normal text-sm" >6 #Kudos</span>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
  </div>
 </template>
