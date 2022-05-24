9aa58187d600dab6afcbacf0b41dcfa1ae3c4694
dragonizado <jesusfrancisco@hotmail.es>
2022-05-24 17:13:02 -0500

    se agregan los cargos al onjeto de los usuarios

--- src/composables/useUsers.js
+++ src/composables/useUsers.js
@@ -2,15 +2,15 @@ import { ref, computed } from 'vue'
 
 export default function useUsers(){
   const personas = ref([
-    { nombre: "Daniel Rubiano", imagen: "/imagenes/JPG/Daniel.jpg", kudos: 0, color:'#ff743f' },
-    { nombre: "Daniela Lozano", imagen: "/imagenes/JPG/Daniela.jpg", kudos: 10, color:'#ed3746' },
-    { nombre: "Emmanuel Deossa", imagen: "/imagenes/JPG/Emma.jpg", kudos: 6, color:'#272162' },
-    { nombre: "Jairo Tulande", imagen: "/imagenes/JPG/Jairo.jpg", kudos: 0, color:'#9340cc' },
-    { nombre: "Jeisson Neira", imagen: "/imagenes/JPG/Jeisson.jpg", kudos: 12, color:'#82cef8' },
-    { nombre: "Jesús Scarpetta", imagen: "/imagenes/JPG/Jesus.jpg", kudos: 0, color:'#fffa6a' },
-    { nombre: "Juan Torres", imagen: "/imagenes/JPG/Juan.jpg", kudos: 3, color:'#1c70ea' },
-    { nombre: "Juliette Hernandez", imagen: "/imagenes/JPG/Juliette.jpg", kudos: 0, color:'#fe61b8' },
-    { nombre:"Kattia Morales", imagen:"/imagenes/JPG/Katt.jpg", kudos: 0, color:'#d45bce' },
+    { nombre: "Daniel Rubiano", cargos: 'Video/Audio Specialist', imagen: "/imagenes/JPG/Daniel.jpg", kudos: 0, color:'#ff743f' },
+    { nombre: "Daniela Lozano", cargos: 'Designer', imagen: "/imagenes/JPG/Daniela.jpg", kudos: 10, color:'#ed3746' },
+    { nombre: "Emmanuel Deossa", cargos: 'Lead Developer', imagen: "/imagenes/JPG/Emma.jpg", kudos: 6, color:'#272162' },
+    { nombre: "Jairo Tulande", cargos: 'Business Development', imagen: "/imagenes/JPG/Jairo.jpg", kudos: 0, color:'#9340cc' },
+    { nombre: "Jeisson Neira", cargos: 'The Chief', imagen: "/imagenes/JPG/Jeisson.jpg", kudos: 12, color:'#82cef8' },
+    { nombre: "Jesús Scarpetta", cargos: 'Full-Stack Developer', imagen: "/imagenes/JPG/Jesus.jpg", kudos: 0, color:'#fffa6a' },
+    { nombre: "Juan Torres", cargos: 'Lead Designer', imagen: "/imagenes/JPG/Juan.jpg", kudos: 3, color:'#1c70ea' },
+    { nombre: "Juliette Hernandez", cargos: 'Junior Designer', imagen: "/imagenes/JPG/Juliette.jpg", kudos: 0, color:'#fe61b8' },
+    { nombre:"Kattia Morales", cargos: 'Chief Hapiness Officer', imagen: "/imagenes/JPG/Katt.jpg", kudos: 0, color:'#d45bce' },
   ])
 
   const crearArrayDeUsuarios = () => {
