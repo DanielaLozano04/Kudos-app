bc2d34013e458fa6949dce0f571349eb11c05388
dragonizado <jesusfrancisco@hotmail.es>
2022-05-29 11:50:02 -0500

    se agrega checkbox al badge de usuarios y efectos visuales

--- src/components/Badge.vue
+++ src/components/Badge.vue
@@ -8,8 +8,13 @@ defineProps({
 </script>
 
 <template>
-  <div class="bg-[#F6F8F9] border rounded-full border-[#D1D5DB] items-center flex">
-      <img class="w-6 h-6 mr-1 rounded-full" :src="data.imagen" :alt="data.nombre">
-      <span class="text-[10px] text-[#6C727F] font-semibold line-clamp-1">{{ data.nombre }}</span>
+<div class="flex items-center ">
+  <input type="checkbox" :id="`chbx-${data.nombre}`" :name="`chbx-${data.nombre}`" class="peer">
+  <div class="bg-[#F6F8F9] peer-checked:bg-[#D1FAE5] text-[#6C727F] peer-checked:text-[#065F46] border rounded-full border-[#D1D5DB] w-full hover:bg-slate-200">
+      <label :for="`chbx-${data.nombre}`" class="flex items-center cursor-pointer">
+        <img class="w-6 h-6 mr-1 rounded-full" :src="data.imagen" :alt="data.nombre">
+        <span class="text-[10px]  font-semibold line-clamp-1">{{ data.nombre }}</span>
+      </label>
   </div>
+</div>
 </template>
