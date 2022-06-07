a3411b15cce6ed98da6088ae51551d48019e9879
dragonizado <jesusfrancisco@hotmail.es>
2022-06-07 16:54:39 -0500

    etiqueta de modal

--- src/components/Iqthinkers.vue
+++ src/components/Iqthinkers.vue
@@ -16,7 +16,7 @@ const { personas } = useUsers()
 		<div class="flex items-center pb-4 place-content-between">
 			<span class="text-sm text-[#6E7C87] font-semibold uppercase"> 🔥 iqthinkers</span>
 			<div>
-					<button 
+					<button
 					type="button"
 					@click="openIQThinkersModal=true"
 					class="bg-white text-[10px] font-semibold text-[#6E7C87] rounded-md border border-[#D1D5DB] px-4 py-[4px]"
@@ -69,9 +69,57 @@ const { personas } = useUsers()
 			</table>
 		</div>
  </div>
- <Modal 
- 		:open-modal="openIQThinkersModal" 
-		@close="openIQThinkersModal = false" 
-		@submit="openIQThinkersModal = false"
-	/>
+
+	<Modal
+		:open-modal="openIQThinkersModal"
+		@close="openIQThinkersModal = false"
+		title="🌟 ADD AN IQTHINKER"
+	>
+		<form class="px-6 py-5" >
+			<div>
+					<label class="block text-sm font-semibold text-[#374151]"> Photo </label>
+					<div class="flex items-center mt-1 mb-6">
+						<span class="inline-block w-12 h-12 overflow-hidden bg-gray-100 rounded-full">
+							<svg class="w-full h-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
+								<path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+							</svg>
+						</span>
+						<button type="button" class="ml-4 bg-white py-1 px-2 border border-gray-300 rounded-md shadow-sm text-xs leading-4 font-medium text-[#374151] hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Change</button>
+					</div>
+				</div>
+
+				<div class="mb-6">
+					<label class="block text-[#374151] text-sm font-semibold mb-1" for="First name">
+						First name
+					</label>
+					<input class=" appearance-none border border-[#D1D5DB] rounded w-full py-2 px-3 text-[#374151] leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" >
+				</div>
+
+				<div class="mb-6">
+					<label class="block text-[#374151] text-sm font-semibold mb-1" for="username">
+						Last Name
+					</label>
+					<input class=" appearance-none border border-[#D1D5DB] rounded w-full py-2 px-3 text-[#374151] leading-tight focus:outline-none focus:shadow-outline" id="Last Name" type="text" >
+				</div>
+
+				<div class="mb-1">
+					<label class="block text-[#374151] text-sm font-semibold mb-1" for="username">
+						Title
+					</label>
+					<input class=" appearance-none border border-[#D1D5DB] rounded w-full py-2 px-3 text-[#374151] leading-tight focus:outline-none focus:shadow-outline" id="Title" type="text" >
+				</div>
+	</form>
+
+<div class="px-6 my-2 mb-2">
+	<button
+		type="button"
+		class="inline-flex justify-center rounded-md border-transparent  px-4 py-2 text-sm font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 hover:bg-[#DC2E20]
+		bg-[#EC3425] border-2 border-[#EC3425] text-white w-full"
+		@click="emit('submit')"
+	>
+		Welcome
+	</button>
+</div>
+
+	</Modal>
 </template>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -10,6 +10,10 @@ import {
 
 defineProps({
     openModal: Boolean,
+    title: {
+      type: String,
+      default: "Titulo por defecto"
+    }
   })
 
 const emit = defineEmits(['submit', 'close'])
@@ -29,11 +33,11 @@ const emit = defineEmits(['submit', 'close'])
       >
         <div class="fixed inset-0 bg-black bg-opacity-25" />
       </TransitionChild>
-      
+
 
       <div class="fixed inset-0 overflow-y-auto">
         <div
-          class="flex min-h-full items-center justify-center p-6 text-center"
+          class="flex items-center justify-center min-h-full p-6 text-center"
         >
           <TransitionChild
             as="template"
@@ -45,61 +49,15 @@ const emit = defineEmits(['submit', 'close'])
             leave-to="opacity-0 scale-95"
           >
             <DialogPanel
-              class="w-full max-w-md transform overflow-hidden rounded bg-white p-7 text-left align-middle shadow-xl transition-all"
+              class="w-full max-w-md overflow-hidden text-left align-middle transition-all transform bg-white rounded shadow-xl p-7"
             >
               <DialogTitle
                 as="h3"
                 class="mx-6 text-lg font-medium leading-6 text-[#111827]"
               >
-                🌟 ADD AN IQTHINKER
-                
+                {{ title }}
               </DialogTitle>
-
-                  <form class="py-5 px-6" >
-                    <div>
-                        <label class="block text-sm font-semibold text-[#374151]"> Photo </label>
-                        <div class="mt-1 flex items-center mb-6">
-                          <span class="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-                            <svg class="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
-                              <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-                            </svg>
-                          </span>
-                          <button type="button" class="ml-4 bg-white py-1 px-2 border border-gray-300 rounded-md shadow-sm text-xs leading-4 font-medium text-[#374151] hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Change</button>
-                        </div>
-                      </div>
-
-                      <div class="mb-6">
-                        <label class="block text-[#374151] text-sm font-semibold mb-1" for="First name">
-                          First name
-                        </label>
-                        <input class=" appearance-none border border-[#D1D5DB] rounded w-full py-2 px-3 text-[#374151] leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" >
-                      </div>
-
-                      <div class="mb-6">
-                        <label class="block text-[#374151] text-sm font-semibold mb-1" for="username">
-                          Last Name
-                        </label>
-                        <input class=" appearance-none border border-[#D1D5DB] rounded w-full py-2 px-3 text-[#374151] leading-tight focus:outline-none focus:shadow-outline" id="Last Name" type="text" >
-                      </div>
-
-                      <div class="mb-1">
-                        <label class="block text-[#374151] text-sm font-semibold mb-1" for="username">
-                          Title
-                        </label>
-                        <input class=" appearance-none border border-[#D1D5DB] rounded w-full py-2 px-3 text-[#374151] leading-tight focus:outline-none focus:shadow-outline" id="Title" type="text" >
-                      </div>                     
-                </form>
-
-              <div class="my-2 px-6 mb-2">
-                <button
-                  type="button"
-                  class="inline-flex justify-center rounded-md border-transparent  px-4 py-2 text-sm font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 hover:bg-[#DC2E20] 
-                  bg-[#EC3425] border-2 border-[#EC3425] text-white w-full"
-                  @click="emit('submit')"
-                >
-                  Welcome
-                </button>
-              </div>
+                  <slot></slot>
             </DialogPanel>
           </TransitionChild>
         </div>
